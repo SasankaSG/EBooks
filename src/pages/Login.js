7d76b361f91f95c7ec44a2af0a52bd44470a4692
Sasanka SG <sasankasg697@gmail.com>
2024-08-25 09:27:59 +0530

    GuestLogin

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -23,6 +23,8 @@ export const Login = () => {
       toast.error(error.message, {closeButton: true, position: "bottom-center"});
     }
   }
+
+  //
   
   async function handleLoginGuest(){
     email.current.value = process.env.REACT_APP_GUEST_LOGIN;
@@ -47,7 +49,7 @@ export const Login = () => {
         <form onSubmit={handleLogin}>
           <div className="mb-6">
               <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-              <input ref={email} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="shubham@example.com" required autoComplete="off" />
+              <input ref={email} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email" required autoComplete="off" />
           </div>
           <div className="mb-6">
               <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
